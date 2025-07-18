8637739703387df3cac5e55a7812fd652b8d8532
0equalboo <ekdldk0118@gmail.com>
2025-07-19 02:36:05 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+
+const server = express();
+const port = 4000
+
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(cors());
+server.use(bodyParser.json());
+
+server.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+server.post("/text", (req, res) => {  //데이터 받는 곳
+  // req
+  const text1 = req.body.inText;
+  console.log(text1);
+  
+  // res
+  const sendText = {
+  	text : "전송 성공!!!",
+  };
+  res.send(sendText);
+});
+
+server.listen(port, () => {
+  console.log(`Example server listening at http://localhost:${port}`);
+});
