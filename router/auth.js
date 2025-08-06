62da93b862c2770f2a7daead7ce3b0cae45595e9
0equalboo <ekdldk0118@gmail.com>
2025-08-06 16:18:52 +0900

    login update4

--- router/auth.js
+++ router/auth.js
@@ -1,33 +1,23 @@
 import express from 'express';
-import { UserModel} from "../schema/user.js";
+// import { UserModel} from "../schema/user.js"; // <-- 이 줄 주석 처리
 import jwt from "jsonwebtoken";
 
-const AuthRouter = Router();
+const AuthRouter = express.Router(); // express.Router()로 수정
 
 AuthRouter.post("/login", async (req, res) => {
-    const data = req.body;
-    const user = await UserModel.findOne({ id : data.id });
-    if (!user) {
-       return res.send("사용자를 찾을 수 없습니다.");
-    }
-    if (user.password != data.password) {
-       return res.send("비밀번호가 틀립니다.");
+    // const data = req.body;
+    // const user = await UserModel.findOne({ id : data.id }); // <-- 이 줄 주석 처리
+    // if (!user) {
+    //    return res.send("사용자를 찾을 수 없습니다.");
+    // }
+    // if (user.password != data.password) {
+    //    return res.send("비밀번호가 틀립니다.");
+    // }
 
-    }
-    const token = jwt.sign(
-        {
-            type: "JWT",
-            id: user.id 
-        },
-        "1234qwer",
-        {
-            expiresIn: "30m",
-            issuer: "Arom", 
-        }
-    );
+    // 지금은 로그인 로직을 임시로 막아두고, 토큰 발급만 테스트
+    const token = jwt.sign( { type: "JWT", id: "testuser" }, "1234qwer", { expiresIn: "30m", issuer: "Arom" });
 
-    return res.send(token);
-
+    return res.send(token);
 });
 
-export default AutoRouter;
+export default AuthRouter; // AutoRouter -> AuthRouter 오타 수정
