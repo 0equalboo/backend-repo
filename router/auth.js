d8312b69205684c8ae033e4e24e673129412ef74
0equalboo <ekdldk0118@gmail.com>
2025-08-14 15:25:21 +0900

    user_login

--- router/auth.js
+++ router/auth.js
@@ -1,10 +1,26 @@
 import express from 'express';
-// import { UserModel} from "../schema/user.js"; // <-- 이 줄 주석 처리
+import { UserModel} from "../schema/user.js"; // <-- 이 줄 주석 처리
 import jwt from "jsonwebtoken";
 
 const AuthRouter = express.Router(); // express.Router()로 수정
 
 AuthRouter.post("/login", async (req, res) => {
+    const { studentId, password} = req.body;
+
+    const user = await UserModel.findOne({ studentId});
+
+    if (!user) {
+        return res.status(404).send("사용자를 찾을 수 없습니다.");
+    }
+    if (user.password !== password) {
+        return res.status(400).send("비밀번호가 틀립니다.");
+    }
+
+    const token = jwt.sign(
+        { id : user.studentId},
+        "1234qwer",
+        {expiresIn: "30m", issuer: "SeManChu"}
+    );
     // const data = req.body;
     // const user = await UserModel.findOne({ id : data.id }); // <-- 이 줄 주석 처리
     // if (!user) {
@@ -15,7 +31,7 @@ AuthRouter.post("/login", async (req, res) => {
     // }
 
     // 지금은 로그인 로직을 임시로 막아두고, 토큰 발급만 테스트
-    const token = jwt.sign( { type: "JWT", id: "testuser" }, "1234qwer", { expiresIn: "30m", issuer: "Arom" });
+    // const token = jwt.sign( { type: "JWT", id: "testuser" }, "1234qwer", { expiresIn: "30m", issuer: "Arom" });
 
     return res.send(token);
 });
