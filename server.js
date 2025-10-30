050e665d74d8b312ee4be0dc81d2b793e401a235
0equalboo <ekdldk0118@gmail.com>
2025-10-31 02:57:41 +0900

    .

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ server.post("/text", (req, res) => {  //데이터 받는 곳
   res.send(sendText);
 });
 
-server.listen(port, '0,0,0,0', () => {
+server.listen(port, '0.0.0.0', () => {
   console.log(`Server listing on port ${port}`);
 });
