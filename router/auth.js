66c3839eb23c29b200aebfe05d810d92baa70c6b
0equalboo <ekdldk0118@gmail.com>
2025-11-06 00:31:47 +0900

    로그인 라우터 수정

--- router/auth.js
+++ router/auth.js
@@ -1,38 +1,41 @@
 import express from 'express';
 import { UserModel } from "../schema/user.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"; // 1. bcrypt 임포트
 
 const AuthRouter = express.Router();
 
 AuthRouter.post("/login", async (req, res) => {
     const { studentId, password } = req.body;
 
-    // 1. (수정) 비밀번호를 제외하고 사용자 정보 조회
-    const user = await UserModel.findOne({ studentId }).select('-password');
+    // 2. (수정) 비밀번호를 포함하여 사용자 정보 조회
+    const user = await UserModel.findOne({ studentId }).select('+password');
 
     if (!user) {
         return res.status(404).send("사용자를 찾을 수 없습니다.");
     }
 
-    // (참고: 실제로는 user.password와 password를 bcrypt로 비교해야 함)
-    // const isMatch = await bcrypt.compare(password, user.password);
-    // if (!isMatch) { ... }
-    
-    // (임시) 텍스트 비밀번호 비교 (이전 코드 기준)
-    const dbUser = await UserModel.findOne({ studentId }); // 비밀번호 포함하여 다시 조회
-    if (dbUser.password !== password) {
+    // 3. (수정) 일반 텍스트 비교 대신 bcrypt.compare 사용
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
         return res.status(400).send("비밀번호가 틀립니다.");
     }
 
+    // 4. (수정) 비밀번호가 일치하면, 응답용 객체에서 비밀번호 제거
+    // (user.password는 delete로 지워지지 않으므로, .toObject() 사용)
+    const userObject = user.toObject();
+    delete userObject.password;
+
     const token = jwt.sign(
-        { id: user.studentId, _id: user._id }, // studentId와 MongoDB _id를 모두 포함
-        "1234qwer", // (보안 경고: 이 키는 .env로 옮겨야 합니다)
+        { id: user.studentId, _id: user._id },
+        "1234qwer", // (보안 경고: .env로 옮기세요)
         { expiresIn: "1h", issuer: "SeManChu" }
     );
 
-    // 2. (수정) 프론트의 AuthResponse 타입에 맞게 객체로 반환
+    // 5. 프론트의 AuthResponse 타입에 맞게 객체로 반환
     return res.status(200).json({
-        user: user,
+        user: userObject, // 비밀번호가 제거된 사용자 객체
         accessToken: token
     });
 });
